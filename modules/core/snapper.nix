8bf329dcac5fd7cffcd5a4309b434ff0afb4ae7a
gooze <gooze@nixos.local>
2026-08-29 16:06:28 +0200

    snapper limits

--- modules/core/snapper.nix
+++ modules/core/snapper.nix
@@ -35,11 +35,21 @@ in
           SUBVOLUME = "/";
           TIMELINE_CREATE = true;
           TIMELINE_CLEANUP = true;
+          TIMELINE_LIMIT_HOURLY = "5";
+          TIMELINE_LIMIT_DAILY = "7";
+          TIMELINE_LIMIT_WEEKLY = "2";
+          TIMELINE_LIMIT_MONTHLY = "1";
+          TIMELINE_LIMIT_YEARLY = "0";
         };
         home = {
           SUBVOLUME = "/home";
           TIMELINE_CREATE = true;
           TIMELINE_CLEANUP = true;
+          TIMELINE_LIMIT_HOURLY = "5";
+          TIMELINE_LIMIT_DAILY = "7";
+          TIMELINE_LIMIT_WEEKLY = "2";
+          TIMELINE_LIMIT_MONTHLY = "1";
+          TIMELINE_LIMIT_YEARLY = "0";
         };
       };
     };
